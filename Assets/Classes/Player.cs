2d4ca74ed1203404d0aac856b0a6c7544ef6eec1
Matthew Ostman <mrostman@syr.edu>
2015-02-10 10:34:32 -0500

    Implementing Player and PlayerToken classe skeletons

--- Assets/Classes/Player.cs
+++ Assets/Classes/Player.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	Vector2 location;
+	Vector2 shiftLocation;
+	Vector2 Motion;
+	int Assignment;
+	enum PositionAbbriviations : byte { QB, F, H, Y, X, Z, C, FG, BG, FT, BT };
+
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
