b4011473f81e2eac7c54bfcb3af87c5849d0ead4
Jason Stewart <JMStewy@yahoo.com>
2015-02-24 13:41:11 -0500

    GameController skeleton

--- Assets/Classes/GameController.cs
+++ Assets/Classes/GameController.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	Play[] allPlays;
+	Play currentPlay;
+	Play userPlay;
+	int timeLeft;
+
+	Play[] playsToUpdate;
+	Play[] userPlaysToUpload;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void NewPlay () {
+
+	}
+
+	Play SelectPlay () {
+
+	}
+
+	void EndPlay () {
+
+	}
 	
+	void EvaluateCorrectness () {
+		
+	}
+	
+	float SRSRecalculate (int correctness, float sinceLastPlayed) {
+		
+	}
+
+	void QueueUpdate () {
+
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
