84bf844375c07c419825446469698c7e63b5fd50
Matthew Ostman <mrostman@syr.edu>
2015-03-02 20:14:14 -0500

    Demo

--- Assets/Classes/Player.cs
+++ Assets/Classes/Player.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Cloudbase;
+using Cloudbase.DataCommands;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour { 
 	Vector2 location;
 	Vector2 shiftLocation;
 	Vector2 Motion;
@@ -12,11 +14,19 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	public void setLocation(Vector3 loc)
+	{
+		location = new Vector2 (loc.x, loc.y);
+	}
+
+	public void setPosition(byte pos)
+	{
+		position = pos;
 	}
 }
--- Assets/Classes/PlayerToken.cs
+++ Assets/Classes/PlayerToken.cs
@@ -4,13 +4,15 @@ using System.Collections;
 [RequireComponent(typeof(BoxCollider))]
 
 public class PlayerToken : MonoBehaviour {
-	private Vector3 screenPoint = new Vector3 (0, 0, 0);
 	private Vector3 offset;
-	private float heightFactor = 20.0f / Screen.height;
-	private int widthFactor = Screen.width;
-	private int gridSize = 0;
+	private float heightFactor = 20.0f / Screen.height; 
+	private int maxX = 7;
+	private int maxY = -9;
 	private Vector3 target;
+	private Vector3 startPoint;
 	private bool snapping = false;
+	private Player player;
+	private int doubleClickCount = 0;
 	Vector3 location;
 	byte position;
 	byte team;
@@ -19,11 +21,20 @@ public class PlayerToken : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		this.gameObject.tag = "token";
 	
 	}
+
+	// FixedUpdate is called every physics frame (50 times/second)
+	void FixedUpdate() {
+		if (doubleClickCount > 0)
+			doubleClickCount--;
+		}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 		// Handle snapping to grid. Target is determined on mouseUp (that is, when the user 'drops' the playerToken
 		float distanceToTarget = Vector3.Distance(this.transform.position, target);
 		if (snapping && distanceToTarget > 0.001f)
@@ -44,14 +55,24 @@ public class PlayerToken : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-		snapping = false;
-		Debug.Log ("Clicked" + Input.mousePosition);
-		offset = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+		// Check for double click
+		Debug.Log (doubleClickCount);
+		if (doubleClickCount > 0)
+						Debug.Log ("DoubleClick");
+		// If not a double click, start dragging the player
+		else
+		{
+			doubleClickCount = 50;
+			startPoint = new Vector3 (this.transform.position.x,
+                         this.transform.position.y,
+                         this.transform.position.z);
+			snapping = false;
+			offset = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+			}
 		}
 	
 	void OnMouseDrag()
 	{
-		Debug.Log ("Dragging");
 		transform.Translate( (Input.mousePosition.x - offset.x) * heightFactor, (Input.mousePosition.y - offset.y) * heightFactor, 0);
 		offset.x = Input.mousePosition.x;
 		offset.y = Input.mousePosition.y;
@@ -60,8 +81,29 @@ public class PlayerToken : MonoBehaviour {
 
 	void OnMouseUp()
 	{
-		snapping = true;
+		// Set snap position
 		target = new Vector3 (Mathf.Round (this.transform.position.x), Mathf.Round (this.transform.position.y), this.transform.position.z);
-	}
 
+		// Check for invalid placement (outside of field of play)
+		if (Mathf.Abs (target.x) > maxX || target.y > -1 || target.y < maxY) 
+		{
+			transform.position = startPoint;
+			return;
+		}
+
+		// Check for invalid placement (overlapping another token)
+		var otherTokens = GameObject.FindGameObjectsWithTag ("token");
+		foreach (var token in otherTokens)
+		{
+			if (token != this.gameObject && Vector3.Distance(target, token.transform.position) < 0.01f)
+			{
+				transform.position = startPoint;
+				return;
+			}
+		}
+
+		// If placement is valid, set snapping so update will move the token
+		//	player.setLocation(target);
+		snapping = true;
+	}
 }	
