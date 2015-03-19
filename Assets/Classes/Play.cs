a85abacf7f5974be99ce354cb267d69e81d9ad1c
josh927 <joshuarodriguez927@gmail.com>
2015-03-19 15:35:17 -0400

    started play class

--- Assets/Classes/Play.cs
+++ Assets/Classes/Play.cs
@@ -2,12 +2,23 @@
 using System.Collections;
 
 public class Play : MonoBehaviour {
+	Player[] players;
+	Player[] defensivePlayers;
+	String name;
+	int parent;
+	float srCoefficient;
+	int playID;
+	int lastSeen;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	void PlayName () {
+		name = currentPlay;
+		}
+
 	// Update is called once per frame
 	void Update () {
 	
