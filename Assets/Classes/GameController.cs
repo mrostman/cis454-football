15dbf8c0f8bfc2e742c2c5c237c6fffb360d8681
Jason Stewart <JMStewy@yahoo.com>
2015-04-12 14:16:49 -0400

    gamecontroller and dbcontroller updates
    finished todos in gamecontroller and databasecontroller

--- Assets/Classes/GameController.cs
+++ Assets/Classes/GameController.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Parse;
 
 public class GameController : MonoBehaviour {
 	// Player Tokens
-	public List<GameObject> offensiveTeam;
-	public List<GameObject> defensiveTeam;
+	public List<PlayerToken> offensiveTeam;
+	public List<PlayerToken> defensiveTeam;
+	public DatabaseController databaseController;
 
-
-	Play[] allPlays;
-	Play currentPlay;
-	Play userPlay;
-	double coefficientTotal;
-	int timeLeft;
+	private ParseObject currentPlay;
+	public System.DateTime playLastSeen;
+	private string playName;
+	private int timeLeft;
+	private float distanceCorrectnessThreshold = 4;
 
 	Queue playsToUpdate = new Queue ();
 	Queue userPlaysToUpload = new Queue ();
@@ -22,74 +23,221 @@ public class GameController : MonoBehaviour {
 	
 	}
 
+	// called when a new round of gameplay begins
 	void NewPlay () {
-		currentPlay = SelectPlay ();
-		userPlay = new Play ();
-		// TODO: display initial player positions on field
-	}
+		currentPlay = databaseController.SelectPlay ();
 
-	Play SelectPlay () {
-		double selectionValue = Random.value*coefficientTotal;
-		int numPlays = allPlays.Length;
-		Play selectedPlay = new Play ();
-		double runningSum;
-		for (int i=0; i<numPlays; i++) 
+		string pName;
+		bool nameResult = currentPlay.TryGetValue("Name", out pName);
+		if (nameResult) 
 		{
-			// TODO: cumulative sum of coefficients from allPlays array
-			// runningSum = runningSum + allPlays[i].getCoefficient ();
-			// if (runningSum > selectionValue)
-			// {
-			//    selectedPlay = allPlays[i];
-			//    break;
-			// }
+			playName = pName;
 		}
-		return selectedPlay;
+
+		ParseObject oTeam;
+		bool oTeamResult = currentPlay.TryGetValue("OffensiveTeam", out oTeam);
+		if (oTeamResult) 
+		{
+			ParseObject oPlayer;
+			string playerKey;
+			for (int i=0;i<11;i++)
+			{
+				playerKey = "Player" + i.ToString();
+				bool oPlayerResult = oTeam.TryGetValue(playerKey, out oPlayer);
+				if (oPlayerResult)
+				{
+					offensiveTeam[i].Initialize(oPlayer);
+				}
+			}
+		}
+
+		ParseObject dTeam;
+		bool dTeamResult = currentPlay.TryGetValue("DefensiveTeam", out dTeam);
+		if (dTeamResult) 
+		{
+			ParseObject dPlayer;
+			string playerKey;
+			for (int i=0;i<11;i++)
+			{
+				playerKey = "Player" + i.ToString();
+				bool dPlayerResult = dTeam.TryGetValue(playerKey, out dPlayer);
+				if (dPlayerResult)
+				{
+					defensiveTeam[i].Initialize(dPlayer);
+				}
+			}
+		}
 	}
 
+	// called to perform cleanup at the end of a round of play
 	void EndPlay () {
-		// TODO: display and animate currentPlay;
+		// reset all PLayerTokens
+		for (int i=0; i<11; i++) 
+		{
+			offensiveTeam[i].deInitialize();
+			defensiveTeam[i].deInitialize();
+		}
+
+		// check correctness and calculate new SRS coefficient
 		int correctness = EvaluateCorrectness ();
-		// TODO: calculate days since last played from timestamp in play wrapper class
-		double sinceLastPlayed = 0.0;
-		double newSRS = SRSRecalculate (correctness,sinceLastPlayed);
-		// TODO: update allPlays entry for currentPlay with new SRS coefficient
-		playsToUpdate.Enqueue (userPlay);
+		float newSRS = SRSRecalculate (correctness,playLastSeen);
+
+		// save user input into a "History" ParseObject to be send to DB
+		ParseObject playHistory = new ParseObject("History");
+		ParseObject historyOTeam = new ParseObject("OffensiveTeam");
+		playHistory["User"] = ParseUser.CurrentUser;
+		string currentPlayID = currentPlay.ObjectId;
+		playHistory["Play"] = ParseObject.CreateWithoutData("Play",currentPlayID);
+		for (int i = 0; i < 11; i++) 
+		{
+			ParseObject historyOPlayer = offensiveTeam[i].getInputParseObject();
+			string fieldName = "Player" + i;
+			historyOTeam.Add(fieldName, historyOPlayer);
+		}
+		playsToUpdate.Enqueue (playHistory);
 		QueueUpdate ();
 	}
-	
+
+	// iterate through all offensive team PlayerTokens and compare the user input data
+	// to the correct data (both sets of data are stored in each PlayerToken)
 	int EvaluateCorrectness () {
-		// TODO: iterate through userPlay.players
-		// for each, iterate through currentPlay.players to find a match on position/type
-		// check location, shift, motion correctness with Vector2.Distance(a,b) < threshhold
+		foreach (PlayerToken oPlayer in offensiveTeam) 
+		{
+			ParseObject inputParsePlayer = oPlayer.getInputParseObject();
+			ParseObject correctParsePlayer = oPlayer.getCorrectParseObject();
+			Vector3 inputLocation;
+			Vector3 correctLocation;
+			Vector2 inputMotion;
+			Vector2 correctMotion;
+			List<Vector2> inputShifts;
+			List<Vector2> correctShifts;
+			ParseObject inputResponsibility;
+			ParseObject correctResponsibility;
+
+			// check correctness of location
+			bool inputLocationResult = inputParsePlayer.TryGetValue("Location", out inputLocation);
+			// Location should always be set, so if no location is found output a debug message
+			if (!inputLocationResult)
+			{
+				string position;
+				bool nameResult = inputParsePlayer.TryGetValue("Position", out position);
+				if (nameResult)
+				{
+					Debug.Log ("error in correctness evaluation, no location input for player " + position);
+				}
+			}
+
+			bool correctLocationResult = correctParsePlayer.TryGetValue("Location", out correctLocation);
+			// Location should always be set, so if no location is found output a debug message
+			if (!correctLocationResult)
+			{
+				string position;
+				bool nameResult = inputParsePlayer.TryGetValue("Position", out position);
+				if (nameResult)
+				{
+					Debug.Log ("error in correctness evaluation, no correct location for player " + position);
+				}
+			}
+
+			float locationDistance = Vector3.Distance (inputLocation, correctLocation);
+			if (locationDistance > distanceCorrectnessThreshold)
+			{
+				return 0;
+			}
+
+			// check correctness of motion
+			bool inputMotionResult = inputParsePlayer.TryGetValue("Motion", out inputMotion);
+			bool correctMotionResult = correctParsePlayer.TryGetValue("Motion", out correctMotion);
+
+			// if one of the two has a motion set and the other doesn't, return incorrect
+			if (inputMotionResult ^ correctMotionResult)
+			{
+				return 0;
+			}
+
+			// if both have a motion, check how far apart they are and return incorrect if it exceeds the threshold
+			if (inputMotionResult && correctMotionResult)
+			{
+				float motionDistance = Vector2.Distance (inputMotion, correctMotion);
+				if (motionDistance > distanceCorrectnessThreshold)
+				{
+					return 0;
+				}
+			}
+
+			// check correctness of shifts
+			bool inputShiftResult = inputParsePlayer.TryGetValue("Shifts", out inputShifts);
+			bool correctShiftResult = correctParsePlayer.TryGetValue("Shifts", out correctShifts);
+
+			// if one of the two has a shift set and the other doesn't, return incorrect
+			if (inputShiftResult ^ correctShiftResult)
+			{
+				return 0;
+			}
+
+			if (inputShiftResult && correctShiftResult)
+			{
+				if(inputShifts.Count != correctShifts.Count)
+				{
+					return 0;
+				}
+
+				for(int i=0;i<inputShifts.Count;i++)
+				{
+					float shiftDistance = Vector2.Distance (inputShifts[i],correctShifts[i]);
+					if (shiftDistance > distanceCorrectnessThreshold)
+					{
+						return 0;
+					}
+				}
+			}
+
+			// check correctness of responsibility
+			bool inputResponsibilityResult = inputParsePlayer.TryGetValue("Responsib", out inputResponsibility);
+			bool correctResponsibilityResult = correctParsePlayer.TryGetValue("Responsib", out correctResponsibility);
+
+			if (inputResponsibilityResult ^ correctResponsibilityResult)
+			{
+				return 0;
+			}
+
+			if (inputResponsibilityResult && correctResponsibilityResult)
+			{
+				if (inputResponsibility.ObjectId != correctResponsibility.ObjectId)
+				{
+					return 0;
+				}
+			}
+		}
 		return 1;
 	}
 	
-	double SRSRecalculate (int correctness, double sinceLastPlayed) {
+	float SRSRecalculate (int correctness, System.DateTime sinceLastPlayed) {
 		// TODO: SRS coefficient product of age component, progress component, effort component
 		// Age A(x) = Cn^x			(x is time elapsed, C is initial constant)
 		// Progress P(x) = Cn^-x	(x is previous success)
 		// Effort {1,10}
-		return  0.0;
+		return  0;
 	}
 
 	void QueueUpdate () {
-
-		while (playsToUpdate.Count > 0)
+		foreach (ParseObject play in playsToUpdate)
 		{
-			// TODO: send playsToUpdate[i] to DatabaseController function
-			// if (return indicates failure) 
-			// {
-			//    break;
-			// }
-			// else 
-			// {
-			//    playsToUpdate.Dequeue();
-			// }
+			bool success = databaseController.UpdatePlay(play);
+			if (success)
+			{
+				playsToUpdate.Dequeue();
+			}
 		}
+	}
+
+	void animationComplete ()
+	{
+		
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
